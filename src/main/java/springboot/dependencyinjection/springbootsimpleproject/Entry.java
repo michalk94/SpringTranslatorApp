2fbd8da2253cda3e8242d3f119a824fde0319b98
michalk94 <michal.kwit94@gmail.com>
2018-11-22 22:16:58 +0100

    Adding lombok adnotations to the Entry class(cause i forgot) responsible for creating constructors @AllArgsConstructor

--- src/main/java/springboot/dependencyinjection/springbootsimpleproject/Entry.java
+++ src/main/java/springboot/dependencyinjection/springbootsimpleproject/Entry.java
@@ -1,12 +1,13 @@
 package springboot.dependencyinjection.springbootsimpleproject;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Entry {
     private String originalWord;
     private String translatedWord;
-
 
 }
